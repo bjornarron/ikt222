5df44a239fdc29ebf7d16fc9df921b3c3d545fff
bjornarfr <bjornarfr@uia.no>
2023-10-19 11:47:33 +0200

    add this

---
+++ assignment2/app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+app.debug = False
+
+DATABASE = 'site.db'
+
+@app.route('/')
+def index():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM posts ORDER BY id DESC')
+    posts = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+@app.route('/post', methods=['POST'])
+def post():
+    title = request.form['title']
+    content = request.form['content']
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+    conn.commit()
+    conn.close()
+    flash('Post added successfully!', 'success')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000)
