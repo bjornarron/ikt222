67f14a64050488b623ae3c125ad362047b7ce86b
Andreas <Andreas.eggelye@gmail.com>
2023-10-31 09:03:07 +0100

    almost finished with assignment 3

--- assignment3/app/app.py
+++ assignment3/app/app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
+from db import init_user_db, register_user, login_user
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
+app.secret_key = 'supersecretkey'  # In a production app, this should be a more secure and hidden key
 app.debug = False
 
-DATABASE = 'database/site.db'
+DATABASE = 'database/site_data.db'
+
+def init_db():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    # Create posts table
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+    # Initialize the user table from db.py
+    init_user_db()
+
+init_db()
 
 @app.route('/')
 def index():
@@ -28,5 +49,34 @@ def post():
     flash('Post added successfully!', 'success')
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if register_user(username, password):
+            flash('Registration successful! Please login.', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('Registration failed.', 'danger')
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if login_user(username, password):
+            flash('Logged in successfully!', 'success')
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your credentials.', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('index'))
+
--- assignment3/app/db.py
+++ assignment3/app/db.py
@@ -1,20 +1,22 @@
 import sqlite3
 import bcrypt
 
-conn = sqlite3.connect('user_data.db')
-cursor = conn.cursor()
+DATABASE = 'database/site_data.db'
 
-# Create a table for users
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
-)
-''')
-conn.commit()
+def init_user_db():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
 
-
+    # Create a table for users
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL
+    )
+    ''')
+    conn.commit()
+    conn.close()
 
 def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
@@ -25,6 +27,8 @@ def check_password(password: str, hashed: bytes) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hashed)
 
 def register_user(username: str, password: str) -> bool:
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     try:
         hashed_password = hash_password(password)
         cursor.execute('INSERT INTO users (username, password_hash) VALUES (?, ?)', (username, hashed_password))
@@ -34,10 +38,15 @@ def register_user(username: str, password: str) -> bool:
         # This error occurs if the username is already taken
         print("Username already exists!")
         return False
+    finally:
+        conn.close()
 
 def login_user(username: str, password: str) -> bool:
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('SELECT password_hash FROM users WHERE username=?', (username,))
     data = cursor.fetchone()
+    conn.close()
     if data:
         stored_hash = data[0]
         return check_password(password, stored_hash)
